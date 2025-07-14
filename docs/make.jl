372e3f84cb8a0f9a2e514e26ac4cc441cd0db68c
Jeffrey Wack <jeffwack111@gmail.com>
2025-07-14 13:35:41 -0700

    updated workflows, readme, and make.jl using PkgTemplates

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,32 @@
-# Inside make.jl
-push!(LOAD_PATH,"../src/")
 using Mandelbrot
 using Documenter
-makedocs(
-         sitename = "Mandelbrot.jl",
-         modules  = [Mandelbrot],
-         pages=[
-                "Home" => "index.md"
-                "Introduction" => "introduction.md"
-                "Kneading sequences" => "kneadingsequence.md"
-                "Dynamic rays" => "dynamicrays.md"
-                "Equipotential lines" => "equipotentials.md"
-                "Finding the angle of an oriented Hubbard Tree" => "embeddedhubbardtree2angle.md"
-                "Hubbard trees" => "hubbardtrees.md"
-                "Images" => "images.md"
-                "Julia sets" => "juliasets.md"
-                "Examples of Hubbard trees" => "smallesttree.md"
-               ])
 
-deploydocs(
+DocMeta.setdocmeta!(Mandelbrot, :DocTestSetup, :(using Mandelbrot); recursive=true)
+
+makedocs(;
+    modules=[Mandelbrot],
+    authors="Jeffrey Wack <jeffwack111@gmail.com> and contributors",
+    sitename="Mandelbrot.jl",
+    format=Documenter.HTML(;
+        canonical="https://jeffwack.github.io/Mandelbrot.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+                "Introduction" => "introduction.md",
+                "Kneading sequences" => "kneadingsequence.md",
+                "Dynamic rays" => "dynamicrays.md",
+                "Equipotential lines" => "equipotentials.md",
+                "Finding the angle of an oriented Hubbard Tree" => "embeddedhubbardtree2angle.md",
+                "Hubbard trees" => "hubbardtrees.md",
+                "Images" => "images.md",
+                "Julia sets" => "juliasets.md",
+                "Examples of Hubbard trees" => "smallesttree.md",
+    ],
+)
+
+deploydocs(;
     repo="github.com/jeffwack/Mandelbrot.jl",
+    devbranch="main",
 )
